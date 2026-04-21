8636b223c59434d2e9dc5a4fec98d095239c4489
Rhishav Lamichhane <110982820+SelAshyn@users.noreply.github.com>
2026-04-21 06:55:18 +0545

    Bugs fixes and favicon updated

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // 🔥 change this from 'media' to 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
